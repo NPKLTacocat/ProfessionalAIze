ca3ed7e767bf52ada4024e54fb53b4f1be6a1d3b
Khang Tran <116851477+NPKLTacocat@users.noreply.github.com>
2025-09-28 01:37:47 -0400

    Set up database connection with postgresql

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,24 @@
 import express from "express";
+import cors from "cors";
 import dotenv from "dotenv";
+import pool from "./db/db.js";
 
 import authRoutes from "./routes/auth.routes.js";
 
 dotenv.config();
+
 const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
 
 app.use("/api/auth", authRoutes);
+app.get("/", async (req, res) => {
+  const result = await pool.query("SELECT current_database()");
+  res.send(`Connected to database: ${result.rows[0].current_database}`);
+});
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
